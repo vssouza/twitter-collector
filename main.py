8f3ece422879ecc645b420188facec0bed75efe7
ezyamvi <vinicius.yamauchi@ericsson.com>
2018-06-12 09:39:21 +0100

    main changes

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from twitter import TweetRetriever
 
-consumer_key = 'mppcFHvYdXtzshV89ppzBmdTR'
-consumer_secret = '9H1OwHwBhHWKQBV8gPj1hbHpTtJF13tHmyzCUArB1DDK5POkPu'
-access_secret = 'Ddc18lswGpBM33IDWezEWsccHyud6Bx4kUH5gP3KQ2rcg'
-access_token = '999273792030507008-2PR0utUvBFHwFebYhOCJt7ATn9FKmSc'
+consumer_key = ''
+consumer_secret = ''
+access_secret = ''
+access_token = ''
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
