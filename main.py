4765a5ef4890413677eb1a085af43d6b89899751
Vinícius de Souza <vs.souza@gmail.com>
2018-06-24 11:58:13 +0100

    Restructuring main file (#3)
    * created config class to load from json
    
    * Refactoring main structure

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import tweepy
+import os
 from tweepy import OAuthHandler
 from tweepy import Stream
 from twitter import TweetRetriever
+from config import ConfigurationLoader, ConfigUtil
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+class Main:
+    def __init__(self):
+        self.config_file_path = os.path.dirname(__file__)
+        self.config_file_path = os.path.join(self.config_file_path, "resources", ConfigUtil.CONFIG_FILENAME)
+        self.config = ConfigurationLoader(self.config_file_path)
+        self.consumer_key = 'YDZ1ka1OjlOpIR8hqx36b28uJ'
+        self.consumer_secret = 'gdFvF9fhLenaR6yj4nkUrpRKQwFb8IrHhXGXdmjSkzdLCcCish'
+        self.access_token = '999273792030507008-JWCIJtlpW2FDAdEeJkpRvD4nKBdWDij'
+        self.access_secret = 'pLp0xdbWbB0Y1zLYKmmzWX2XcBg8qiDcLEZz355V3uPHn'
 
-api = tweepy.API(auth)
+    def run(self):
+        auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_secret)
+        tweepy.API(auth)
+        tweet_retriever = TweetRetriever(self.consumer_key, self.consumer_secret, self.access_secret, self.access_secret)
+        twitter_stream = Stream(auth, tweet_retriever)
+        twitter_stream.filter(track=['#fortnite'], languages=['en'])
 
-tweet_retriever = TweetRetriever(consumer_key, consumer_secret, access_secret, access_secret)
 
-twitter_stream = Stream(auth, tweet_retriever)
-twitter_stream.filter(track=['#fortnite'], languages=['en'])
+main = Main()
+main.run()
