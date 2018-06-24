14c4ae9f179cceb8ffec648442c73c773fea895f
Vinicius Yamauchi <vs.souza@gmail.com>
2018-06-24 23:05:28 +0100

    Added file log support

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ class Main:
         self._config_file_path = os.path.dirname(__file__)
         self._config_file_path = os.path.join(self._config_file_path, "resources", ConfigUtil.CONFIG_FILENAME)
         self._config = ConfigurationLoader(self._config_file_path)
-        self._consumer_key = 'YDZ1ka1OjlOpIR8hqx36b28uJ'
-        self._consumer_secret = 'gdFvF9fhLenaR6yj4nkUrpRKQwFb8IrHhXGXdmjSkzdLCcCish'
-        self._access_token = '999273792030507008-JWCIJtlpW2FDAdEeJkpRvD4nKBdWDij'
-        self._access_secret = 'pLp0xdbWbB0Y1zLYKmmzWX2XcBg8qiDcLEZz355V3uPHn'
+        self._consumer_key = ''
+        self._consumer_secret = ''
+        self._access_token = ''
+        self._access_secret = ''
 
     def run(self):
         auth = OAuthHandler(self._consumer_key, self._consumer_secret)
