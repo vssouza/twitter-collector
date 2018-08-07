c2e0d01b6361fa356d52dd849f1b9ae17c7fa7c0
ezyamvi <vinicius.yamauchi@ericsson.com>
2018-08-07 11:16:31 +0100

    changed to use json config file for twitter info

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ class Main:
         self._config_file_path = os.path.dirname(__file__)
         self._config_file_path = os.path.join(self._config_file_path, "resources", ConfigUtil.CONFIG_FILENAME)
         self._config = ConfigurationLoader(self._config_file_path)
-        self._consumer_key = ''
-        self._consumer_secret = ''
-        self._access_token = ''
-        self._access_secret = ''
+        self._twitter_config = self._config.get_twitter_config()
+        self._consumer_key = self._twitter_config["consumerkey"]
+        self._consumer_secret = self._twitter_config["consumersecret"]
+        self._access_token = self._twitter_config["accesstoken"]
+        self._access_secret = self._twitter_config["accesssecret"]
+        self._hash_tag = self._twitter_config["hashtag"]
+        self._language = self._twitter_config["language"]
 
     def run(self):
         auth = OAuthHandler(self._consumer_key, self._consumer_secret)
@@ -22,7 +25,7 @@ class Main:
         tweepy.API(auth)
         tweet_retriever = TweetRetriever(self._consumer_key, self._consumer_secret, self._access_secret, self._access_secret)
         twitter_stream = Stream(auth, tweet_retriever)
-        twitter_stream.filter(track=['#fortnite'], languages=['en'])
+        twitter_stream.filter(track=[self._hash_tag], languages=[self._language])
 
 
 main = Main()
